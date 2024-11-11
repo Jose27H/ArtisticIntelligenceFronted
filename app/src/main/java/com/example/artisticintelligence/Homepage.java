c968b5b506e3a72cfe3560a35772b0550278c38a
Jose <josehilario27@hotmail.com>
2024-11-11 13:11:40 -0500

    initial

---
+++ app/src/main/java/com/example/artisticintelligence/Homepage.java
@@ -0,0 +1,34 @@
+package com.example.artisticintelligence;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_homepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Button button = findViewById(R.id.button);
+            button.setOnClickListener(this::onClick);
+            return insets;
+        });
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(Homepage.this, "Pressed", Toast.LENGTH_SHORT).show();
+    }
+
+}
