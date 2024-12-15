fed257528dcded501f4857063899e63b02ca0553
Jose27H <107864447+Jose27H@users.noreply.github.com>
2024-12-15 13:34:47 -0500

    added project to google cloud

--- app/src/main/java/com/example/artisticintelligence/Homepage.java
+++ app/src/main/java/com/example/artisticintelligence/Homepage.java
@@ -22,18 +22,11 @@ public class Homepage extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
 
-        setupWindowInsets();
         initializeViews();
         initializeNetworkSender();
     }
 
-    private void setupWindowInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
 
     private void initializeViews() {
         sendButton = findViewById(R.id.button);
